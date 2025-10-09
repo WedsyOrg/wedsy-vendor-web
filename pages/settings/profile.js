9a076c0dcac123e39f61352e276e13d49d0cfd69
Ayush <ayushkumar85385@gmail.com>
2025-10-09 10:52:02 +0530

    fixed gallery section

--- pages/settings/profile.js
+++ pages/settings/profile.js
@@ -3061,7 +3061,7 @@ export default function Settings({ user }) {
             <div className="p-4 border-b border-gray-200">
               <div className="flex items-center justify-between">
                 <h3 className="text-lg font-semibold text-black">
-                  Cover Photo
+                  Crop Photo
                 </h3>
                 <button
                   onClick={() => {
